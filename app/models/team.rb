25bb8489b8ddcb9eaa552382284a78890f25cb78
Juan Tenezaca <tenj34@gmail.com>
2018-10-20 18:08:33 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,13 +1,12 @@
-<h1> Teams</h1>
-<div class="team">
-<h3>Team Name: <%= @team.name %> </h3>
-<h3>Team Motto: <%= @team.motto %> </h3>
-</div><br>
- <h1> Heroes </h1>
-<div class="super_hero">
-<% @superheroes.each do |superhero| %>
-<h3>Hero Name: <%= superhero.name %> </h3>
-<h3>Hero Power: <%= superhero.power %> </h3>
-<h3>Hero Biography: <%= superhero.bio %> </h3>
- </div><br>
-<%end%>
+class Team
+   @@teams =[]
+   attr_reader :name, :motto
+   def initialize(params)
+    @name = params[:name]
+    @motto = params[:motto]
+    @@teams << self
+  end
+   def self.all
+    @@teams
+  end
+ end
